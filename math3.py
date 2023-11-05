47c8861d5a7b70fba3fd44434e4cf017a40ca6c5
whypxralich <whypxrlch@gmail.com>
2023-11-05 21:32:28 +0300

    Create math3.py

---
+++ math3.py
@@ -0,0 +1,14 @@
+print ("Enter first number")
+n1 = int(input())
+print ("Enter second number")
+n2 = int(input())
+amm = n1 + n2
+diff = n1 - n2
+delit = n1 / n2
+umn = n1 * n2
+
+print (n1, "+", n2, "=", amm)
+print (n1, "-", n2, "=", diff)
+print (n1, "/", n2, "=", delit)
+print (n1, "*", n2, "=", umn)
+
